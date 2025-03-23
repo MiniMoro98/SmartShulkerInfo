0e9df9b966e0a488423eac91a1d97bf228e1c174
MiniMoro98 <moro18391@gmail.com>
2025-03-23 19:45:47 +0100

    Aggiornamento

--- src/main/java/it/moro/smartShulkerInfo/SmartShulkerInfo.java
+++ src/main/java/it/moro/smartShulkerInfo/SmartShulkerInfo.java
@@ -12,6 +12,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,11 +23,10 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public final class SmartShulkerInfo extends JavaPlugin implements Listener, CommandExecutor {
 
@@ -48,21 +48,47 @@ public final class SmartShulkerInfo extends JavaPlugin implements Listener, Comm
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        if(sender instanceof Player player){
-            if(command.getName().equalsIgnoreCase("shulkerinfo")){
-                if(args.length > 0){
-                   if(args[0].equalsIgnoreCase("reload")){
-                       if(player.hasPermission("shulkerinfo.reload")){
-                           loadFiles();
-                           player.sendMessage("§aConfiguration reloaded!");
-                           return true;
-                       }
-                   }
+        if (sender instanceof Player player) {
+            if (command.getName().equalsIgnoreCase("shulkerinfo")) {
+                if (args.length > 0) {
+                    if (args[0].equalsIgnoreCase("reload")) {
+                        if (player.hasPermission("shulkerinfo.reload")) {
+                            loadFiles();
+                            player.sendMessage("§aConfiguration reloaded!");
+                            return true;
+                        }
+                    } else if (args[0].equalsIgnoreCase("killarmorstand")) {
+                        List<Entity> entities = new ArrayList<>(Bukkit.getWorlds().getFirst().getEntities());
+                        for (Entity entity : entities) {
+                            if (entity instanceof ArmorStand armorStand) {
+                                if (!armorStand.isVisible() && armorStand.isCustomNameVisible()) {
+                                    armorStand.remove();
+                                }
+                            }
+                        }
+                        player.sendMessage("§aAll armorstands have been removed");
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(command.getName().equalsIgnoreCase("shulkerinfo")) {
+            Player player = (Player) sender;
+            if (player.hasPermission("shulkerinfo.killarmorstand")) {
+                if (args.length == 1) {
+                    return Arrays.asList("reload", "killarmorstand");
+                }
+                return Collections.emptyList();
+            }
+        }
+        return null;
+    }
+
 
     private void createDataFolder() {
         if (!getDataFolder().exists() && !getDataFolder().mkdirs()) {
@@ -86,7 +112,7 @@ public final class SmartShulkerInfo extends JavaPlugin implements Listener, Comm
     public void onShulkerBoxClick(PlayerInteractEvent event) {
         if (event.getAction().toString().contains("LEFT_CLICK") && event.getClickedBlock() != null) {
             Player player = event.getPlayer();
-            if(config.getBoolean("requires-free-hand")) {
+            if (config.getBoolean("requires-free-hand")) {
                 if (player.getInventory().getItemInMainHand().getType() == Material.AIR) {
                     Block block = event.getClickedBlock();
                     if (block.getType() == Material.SHULKER_BOX) {
@@ -125,7 +151,7 @@ public final class SmartShulkerInfo extends JavaPlugin implements Listener, Comm
         Inventory inventory = shulkerBox.getInventory();
         int items = config.getInt("Chat.amount-item");
         player.sendMessage(" \n \n ");
-        if(!Objects.requireNonNull(config.getString("Chat.tab")).isEmpty()){
+        if (!Objects.requireNonNull(config.getString("Chat.tab")).isEmpty()) {
             String tab = Objects.requireNonNull(config.getString("Chat.tab"))
                     .replaceAll("&", "§");
             player.sendMessage(tab);
@@ -145,11 +171,11 @@ public final class SmartShulkerInfo extends JavaPlugin implements Listener, Comm
                 air++;
             }
         }
-        if(air == items){
-                String empty = Objects.requireNonNull(config.getString("Chat.empty")).replaceAll("&", "§");
-                player.sendMessage(empty);
+        if (air == items) {
+            String empty = Objects.requireNonNull(config.getString("Chat.empty")).replaceAll("&", "§");
+            player.sendMessage(empty);
         }
-        if(!Objects.requireNonNull(config.getString("Chat.end-tab")).isEmpty()){
+        if (!Objects.requireNonNull(config.getString("Chat.end-tab")).isEmpty()) {
             String endtab = Objects.requireNonNull(config.getString("Chat.end-tab"))
                     .replaceAll("&", "§");
             player.sendMessage(endtab);
@@ -184,7 +210,7 @@ public final class SmartShulkerInfo extends JavaPlugin implements Listener, Comm
                 air++;
             }
         }
-        if(items == air){
+        if (items == air) {
             ArmorStand armorStand = player.getWorld().spawn(location.clone()
                     .add(0.5, -1 + 1 * 0.25, 0.5), ArmorStand.class);
             armorStand.setVisible(false);
